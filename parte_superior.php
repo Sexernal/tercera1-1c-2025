38d6efc60ac9338c33dd821977e833468b522b3e
Sexernal <salazarjocksan2005@gmail.com>
2025-02-22 14:24:16 -0600

    Primer commit de mi proyecto

---
+++ parte_superior.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="Spanish">
+
+<head>
+<meta charset="utf-8">
+<meta name="Proyecto Tercera" content="Proyecto Tercera"> 
+<link rel="stylesheet" href="./css/main.css">
+<link rel="icon" type="image.jpg" href="img/new-rocket.png">
+<title>Proyecto Tercera</title>
+</head>
+
+
+<menu>
+
+<div class="menu-up">
+    <a href="./">
+    <img src="./img/new-rocket.png" alt="" height:70px>
+    </a>
+</div>
+ <div class="menu-down">
+    <div class ="menu-izquierda">
+        <a href="">News</a>
+        <a href="">Discord Webhook</a>
+        <a href="">Database</a>
+        <a href="">Tolls</a>
+        <a href="">Guides</a>
+        <a href="">Comunity</a>
+        <a href="">Premium</a>
+    </div>
+
+    <div class="menu-derecha">
+    <img src="./img/pantalla-completa.png" alt="">
+        <img src="./img/campana.png" alt="">
+        <img src="./img/circulo.png" alt="">
+        <img src="./img/mensaje.png" alt="">
+        
+        
+    </div>
+
+ </div>
+
+</menu>
+
+
